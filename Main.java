a219f0783ee68e837a8451ddf745b9c562d6e03d
a6400 <wagnamagdefense@gmail.com>
2025-02-03 10:57:50 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main{
   public static void main (String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("1.Unisex \n 2.Women \n 3.Men \n 4.Children \n 5.Infants \n Pick a category:"
+    
     System.out.println("Kupal ka lyndo");
   }
 }
