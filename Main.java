a515f9e3e62a4a0736e0ebe7b937a14af1df1ece
a6400 <wagnamagdefense@gmail.com>
2025-02-03 11:00:13 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@ public class Main{
   public static void main (String[] args){
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("1.Unisex \n 2.Women \n 3.Men \n 4.Children \n 5.Infants \n Pick a category:");
+    System.out.println("1.Unisex \n2.Women \n3.Men \n4.Children \n5.Infants \nPick a category:");
+
+    String category = scanner.nextLine();
+
+    scanner.close();
     
     System.out.println("Kupal ka lyndo");
   }
