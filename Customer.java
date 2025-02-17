9a4aa0705f9ee43d37458f6f5ccd3634700e3d7c
Bridge-jpeg <lyndolusac@gmail.com>
2025-02-17 09:33:30 +0800

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,62 @@
+public class Customer{
+	private String name;
+	public void setName(String n) {
+		name=n;
+		return n;
+	}
+	
+	public String getName() {
+		return n;
+	}
+	
+	private String address;
+	public void setAddress(String a) {
+		address=a;
+		return a;
+	}
+	
+	public String getAddress() {
+		return a;
+	}
+	
+	private String ID;
+	public void setID(String iD) {
+		ID=iD;
+		return iD;
+	}
+	
+	public String getID() {
+		return iD;
+	}
+	
+	private String emailAddress;
+	public void setEmailAddress(String e) {
+		emailAddress=e;
+		return e;
+	}
+	
+	public String getEmailAddress() {
+		return e;
+	}
+	
+	private String phoneNumber;
+	public void setPhoneNumber(String p) {
+		phoneNumber=p;
+		return p;
+	}
+	
+	public String getPhoneNumber() {
+		return p;
+	}
+	
+	private Catalog catalog;
+	public void setCatalog(Catalog c) {
+		catalog=c;
+		return c;
+	}
+	
+	public Catalog getCatalog() {
+		return c;
+	)
+	}
+}
