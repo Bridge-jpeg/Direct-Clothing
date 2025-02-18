eb9a61354801f61055867089743233412e402f90
ZeitAPC <fdanciro@student.apc.edu.ph>
2025-02-18 10:42:30 +0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,4 +1,8 @@
 package Main;
+/*
+For storing the info of customers instead
+Info entered through main is stored here, and is called by payment and csr classes
+*/
 
 public class Customer {
     String userFirstName;
