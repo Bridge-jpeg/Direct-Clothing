ed6bf45119529879d773b35efe34e9a57ebec6e5
ZeitAPC <fdanciro@student.apc.edu.ph>
2025-02-20 09:50:11 +0800

    Update CSR.java

--- CSR.java
+++ CSR.java
@@ -5,15 +5,15 @@ Contains the variables for Ticket ID and the issue entered
 Pulls from User class
 */
 public class CSR {
-    String csrID = "1231237129";
+    public int csrID = (int)(Math.random()*2147483647);
     public String csrIssue;
 
-    public String printSupportTicket(String userFirstName, String userLastName, String csrIssue){
+    public String print(String userFirstName, String userLastName, String csrIssue, int csrID){
         System.out.println(" ");
         System.out.println("===== Customer Support =====");
         System.out.println("Name:"+userFirstName +" "+ userLastName);
         System.out.println("Issue: " + csrIssue);
-        System.out.println("Ticket ID: "+csrID);
+        System.out.println("Ticket ID: "+ csrID);
         return             "=============================";
     }
 
