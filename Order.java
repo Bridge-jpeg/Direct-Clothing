145d4653ae3ca04ae7152c0434af14baf74fb753
ZeitAPC <fdanciro@student.apc.edu.ph>
2025-02-20 09:53:58 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,63 +1,21 @@
-public class Order{
-	private String orderId;
-	public void setOrderId(String s) {
-		orderId=s;
-		return s;
-	}
-	
-	public String getId() {
-		return s;
-	}
-	
-	private Customer customer;
-	public void setCustomer(Customer c) {
-		customer=c;
-		return;
-	}
-	
-	public Customer getCustomer() {
-		return c;
-	}
-	
-	private OrderForm form;
-	public void setForm(OrderForm f) {
-		form=f;
-		return f;
-	}
-	
-	public OrderForm getForm() {
-		return=f;
-	}
-	
-	private String orderStatus;
-	public void setOrderStatus(String s) {
-		orderStatus=s;
-		return s;
-	}
-	
-	public  String getOrderStatus() {
-		return s;
-	}
-	
-	private String totalPrice;
-	public void setTotalPrice(String p) {
-		totalPrice=p;
-		return p;
-	}
-	
-	public String getTotalPrice() {
-		return p;
-	}
-	
-	private String paymentVerification;
-	public void setPaymentVerification(String v) {
-		paymentVerification=v;
-		return v;
-	}
-	
-	public String getPaymentVerification() {
-		return v;
-	} 
+package Main;
+
+public class Order {
+    String itemName;
+    int itemQuantity;
+    double itemPrice;
+    //    Returns the receipt to the user after ordering
+    public void receiptToUser(String userFirstName, String userLastName, String userAddress, String userContactNumber, String itemName, double itemPrice, int itemQuantity, String paymentType, int paymentID){
+        System.out.println(" ");
+        System.out.println("===========Receipt===========");
+        System.out.println("Name:"+userFirstName +" "+ userLastName);
+        System.out.println("Address: " + userAddress);
+        System.out.println("Contact number: "+userContactNumber);
+        System.out.println("Item: " + itemName);
+        System.out.println("Price: " + itemPrice*itemQuantity);
+        System.out.println("Quantity: " + itemQuantity);
+        System.out.println("Payment type: " + paymentType);
+        System.out.println("Payment ID: " + paymentID);
+        System.out.println( "=============================");
+    }
 }
-		
-	
