96a9dde7e4a53ad7e1396608d308ad9c4be44355
ZeitAPC <fdanciro@student.apc.edu.ph>
2025-02-24 22:59:56 +0800

    Revised main
    To be implemented 
    - new catalog system 
    - csr system
    - choose an item system

--- Main.java
+++ Main.java
@@ -1,136 +1,111 @@
-package Main;
-import Item.forFemale;
-import Item.forMale;
+import ForImplementation.SpringCatalog;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        CSR csr = new CSR();
-        Customer user = new Customer();
-        Payment payment = new Payment();
-        Inventory inventory = new Inventory();
-        forMale male = new forMale();
-        forFemale female = new forFemale();
-        Order order = new Order();
-
         Scanner input = new Scanner(System.in);
-
-        int action;
+        Customer customer = new Customer();
+        currentCatalog springCatalog = new testCatalog();
+        Inventory inventory = new Inventory();
+        Order order = new Order();
+        Payment payment = new Payment();
+        SpringCatalog item1 = new SpringCatalog("1: Linen Button-Down Shirt", 2800 , 25);
 /*
-        This code block is for entering the information of the user
-        Includes: First and Last name, Address, Email Address, Contact number
- */
-        System.out.println("=============================");
-        System.out.print("Enter first name: ");
-        user.userFirstName = input.nextLine();
+        To be implemented
+        SpringCatalog item1 = new SpringCatalog("1: Linen Button-Down Shirt", 2800 , 25);
+        SpringCatalog item2 = new SpringCatalog("2: Slim-Fit Chino Pants", 3360, 30);
+        SpringCatalog item3 = new SpringCatalog("3: Casual Loafers", 4480, 0);
 
-        System.out.print("Enter last name: ");
-        user.userLastName = input.nextLine();
+        WinterCatalog item4 = new WinterCatalog("4: Wool Overcoat", 10080, 10);
+        WinterCatalog item5 = new WinterCatalog("5: Thermal Knit Sweater", 3920, 20);
+        WinterCatalog item6 = new WinterCatalog("6: Leather Lace-Up Boots", 6720 , 15);
 
-        System.out.print("Enter address: ");
-        user.userAddress = input.nextLine();
+        SpringCatalog [] items = {item1, item2, item3};
+        WinterCatalog [] items2 = {item4, item5, item6};
+
+
+
+        for (SpringCatalog i:items){
+            System.out.println("Spring Collection - Fresh & Casual");
+            i.itemInfo();
+        }
+        for (WinterCatalog x:items2){
+            System.out.print("Winter Collection ️- Cozy & Functional");
+            x.itemInfo();
+        }
+*/
+
+//       Test variables, to be transferred to other class
+        int testChooseGender;
+        int chooseACatalog;
+
+//      For entering customer information
+//      To be printed on the receipt
+        System.out.println("====== User form ======");
+        System.out.print("Enter name: ");
+        customer.userName = input.nextLine();
+
+        System.out.print("Enter Address: ");
+        customer.userAddress = input.nextLine();
 
         System.out.print("Enter contact number: ");
-        user.userContactNumber = input.nextLine();
+        customer.userContactNumber = input.nextLine();;
+        System.out.println("=======================");
 
-        System.out.println("=============================");
+        System.out.println("1: Male\n2: Female");
+        System.out.print("Choose: ");
+        testChooseGender = input.nextInt();
 
-        System.out.println("\n=============================");
-        System.out.println("1: Shop");
-        System.out.println("2: Customer support");
-        System.out.print("Enter action: ");
-        action = input.nextInt();
-        System.out.println("=============================");
-/*
-        Shop function
-        Allows users to place their orders and choose between payment methods
-        Shop function is almost complete
- */
-        if (action == 1){
-            System.out.println("Shop");
+        if (testChooseGender == 1){
+            System.out.println("\n1: Spring Catalog - Fresh and Casual \uD83C\uDF3F\uD83C\uDF1E");
+            System.out.print("Pick a catalog: ❄ \uFE0F\uD83E\uDDE5");
+            chooseACatalog = input.nextInt();
 
-            System.out.println("\n1: Male");
-            System.out.println("2: Female");
-            System.out.print("Select: ");
-            inventory.genderSelect = input.nextInt();
+            switch (chooseACatalog){
+                case 1:
+                    System.out.println(" ");
+                    inventory.displayCatalog(testCatalog.getSpringCatalogItem(), testCatalog.getSpringCatalogPrice(), springCatalog.isCurrent());
+                    System.out.print("Choose an item: ");
+                    inventory.chooseItem = input.nextInt();
+                    System.out.println("\n"+inventory.showItemInfo(inventory.chooseItem, testCatalog.getSpringCatalogItem(), testCatalog.getSpringCatalogItemStock()));
+                    order.itemName = inventory.showItemName(inventory.chooseItem, testCatalog.getSpringCatalogItem());
+                    order.itemPrice = inventory.showItemPrice(inventory.chooseItem, testCatalog.getSpringCatalogPrice());
 
-            if (inventory.genderSelect == 1){
-                System.out.println("\nMale");
-                System.out.println("Top or Bottom clothes");
-                System.out.println("1. Top \n2: Bottom");
-                System.out.print("Choose: ");
-                inventory.topOrBottom = input.nextInt();
-
-//                This is for displaying and ordering the items for male
-                if (inventory.topOrBottom ==1){
-                    inventory.displayItemAndPrice(male.getForMaleTop(), male.getForMaleTopPrice());
-                    System.out.print("Choose Item: ");
-                    int chooseItem = input.nextInt();
-                    System.out.println("\n"+inventory.order(chooseItem, male.getForMaleTop())+ ": " + inventory.orderPrice(chooseItem, male.getForMaleTopPrice()));
-                    order.itemName = inventory.order(chooseItem, male.getForMaleTop());
-                    order.itemPrice = inventory.orderPrice(chooseItem, male.getForMaleTopPrice());
-                    System.out.print("Item quantity: ");
-                    order.itemQuantity = input.nextInt();
+                    if(inventory.ifInStock(inventory.chooseItem, testCatalog.getSpringCatalogItemStock())){
+                        System.out.print("Enter item quantity: ");
+                        order.itemQuantity = input.nextInt();
+                        System.out.println("Proceed to check out");
+                        break;
                 }
-                if (inventory.topOrBottom ==2){
-                    inventory.displayItemAndPrice(male.getForMaleBottom(), male.getForMaleBottomPrice());
-                    System.out.print("Choose Item: ");
-                    int chooseItem = input.nextInt();
-                    System.out.println("\n"+inventory.order(chooseItem, male.getForMaleBottom())+ ": " + inventory.orderPrice(chooseItem, male.getForMaleBottomPrice()));
-                    order.itemName = inventory.order(chooseItem, male.getForMaleBottom());
-                    order.itemPrice = inventory.orderPrice(chooseItem, male.getForMaleBottomPrice());
-                    System.out.print("Item quantity: ");
-                    order.itemQuantity = input.nextInt();
-                }
+                    System.out.println("This item is out of stock, do you want to pre order?");
+
             }
-
-//          This is for displaying and ordering the items for female
-            if (inventory.genderSelect == 2){
-                System.out.println("\nFemale");
-                System.out.println("Top or Bottom clothes");
-                System.out.println("1. Top \n2: Bottom");
-                System.out.print("Choose: ");
-                inventory.topOrBottom = input.nextInt();
-
-                if (inventory.topOrBottom ==1){
-                    inventory.displayItemAndPrice(female.getFemaleTop(), female.getFemaleTopPrice());
-                    System.out.print("Choose Item: ");
-                    int chooseItem = input.nextInt();
-                    System.out.println("\n"+inventory.order(chooseItem, female.getFemaleTop())+ ": " + inventory.orderPrice(chooseItem, female.getFemaleTopPrice()));
-                    order.itemName = inventory.order(chooseItem, male.getForMaleTop());
-                    order.itemPrice = inventory.orderPrice(chooseItem, female.getFemaleTopPrice());
-                    System.out.print("Item quantity: ");
-                    order.itemQuantity = input.nextInt();
-                }
-                if (inventory.topOrBottom ==2){
-                    inventory.displayItemAndPrice(female.getFemaleBottom(), female.getFemaleBottomPrice());
-                    System.out.print("Choose Item: ");
-                    int chooseItem = input.nextInt();
-                    System.out.println("\n"+inventory.order(chooseItem, female.getFemaleBottom())+ ": " + inventory.orderPrice(chooseItem, female.getFemaleBottomPrice()));
-                    order.itemName = inventory.order(chooseItem, female.getFemaleBottom());
-                    order.itemPrice = inventory.orderPrice(chooseItem, female.getFemaleBottomPrice());
-                    System.out.print("Item quantity: ");
-                    order.itemQuantity = input.nextInt();
-                }
-            }
-
-//          This is for selecting the mode of payment for the user
-
-            System.out.println("\n1: Cash on Delivery");
-            System.out.println("2: Online Payment");
-            System.out.print("Select Payment method: ");
-
-            payment.paymentTypeNum = input.nextInt();
-            payment.setPaymentTypeNum(payment.paymentTypeNum);
-
-//          This prints the receipt to the user
-            order.receiptToUser(user.userFirstName, user.userLastName, user.userAddress, user.userContactNumber, order.itemName, order.itemPrice, order.itemQuantity, payment.paymentType, payment.paymentID);
         }
-        if (action == 2){
-            System.out.print("What's your concern: ");
-            input.nextLine();
-            csr.csrIssue = input.nextLine();
-            System.out.println(csr.print(user.userFirstName, user.userLastName, csr.csrIssue, csr.csrID));
+        System.out.println("\n===== Payment =====");
+        System.out.print("1: Cash on Delivery\n2: Credit Card\n3: Pay by Check (eCheck)\nChoose mode of payment: ");
+        payment.modeOfPayment = input.nextInt();
+
+        switch (payment.modeOfPayment){
+            case 1:
+                break;
+            case 2:
+                System.out.print("Enter bank name: ");
+                input.nextLine();
+                payment.bankName = input.nextLine();
+
+                System.out.print("Enter Credit card number: ");
+                payment.accountNumber = input.nextLine();
+                break;
+            case 3:
+                System.out.print("Enter bank name: ");
+                input.nextLine();
+                payment.bankName = input.nextLine();
+
+                System.out.print("Enter Checking account number: ");
+                payment.accountNumber = input.nextLine();
+                break;
         }
+        order.printReceipt(customer.userName, customer.userAddress, customer.userContactNumber, order.itemName, order.itemPrice, order.itemQuantity, payment.paymentType(payment.modeOfPayment, payment.bankName, payment.accountNumber), payment.paymentID);
     }
 }
